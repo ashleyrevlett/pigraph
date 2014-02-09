8d94420fe97aea1486248376503ae053c21c0a59
ashley revlett <ashleyrevlett@gmail.com>
2014-02-09 15:17:03 -0500

    initial commit of new project name and structure

---
+++ pigraph/display/node.py
@@ -0,0 +1,118 @@
+import pygame
+from pygame.locals import *
+from vector import *
+
+
+class Node(pygame.sprite.Sprite):
+    
+    def __init__(self, index=0, x=0, y=0):
+        '''
+        Class:
+            creates a sprite
+        Parameters:
+            - self
+            - index
+                index to use when accessing graph vertex sequence
+            - x, y
+                starting x and y pos to draw the node at
+        '''
+        # self.image = pygame.image.load("images/ornament_001.png").convert() # load image
+        # self.rect = self.image.get_rect()
+
+        self.radius = 4
+        self.rect = Rect(0,0, self.radius*2, self.radius*2)
+        self.trueX = x # created because self.rect.center does not hold
+        self.trueY = y # decimal values but these do
+        self.rect.center = (self.trueX, self.trueY) # set starting position
+        self.speed = 5 # movement speed of the sprite
+        self.speedX = 0 # speed in x direction
+        self.speedY = 0 # speed in y direction
+        self.normal_friction = .9 # friction while accelerating; lower = less springiness
+        self.slowing_friction = .3 # friction while slowing down; lower = slows down faster
+        self.index = index # used to access edgesequence
+        self.target = None # starts off with no target
+
+    def get_direction(self, target):
+        '''
+        Function:
+            takes total distance from sprite.center
+            to the sprites target
+            (gets direction to move)
+        Returns:
+            a normalized vector
+        Parameters:
+            - self
+            - target
+                x,y coordinates of the sprites target
+                can be any x,y coorinate pair in
+                brackets [x,y] or parentheses (x,y)
+        '''
+        if self.target: # if the square has a target
+            position = Vector(self.trueX, self.trueY) # create a vector from center x,y value
+            target = Vector(target[0], target[1]) # and one from the target x,y
+            self.dist = target - position # get total distance between target and position
+
+            direction = self.dist.normalize() # normalize so its constant in all directions
+            return direction
+        
+    def distance_check(self, dist):
+        '''
+        Function:
+            tests if the total distance from the
+            sprite to the target is smaller than the
+            ammount of distance that would be normal
+            for the sprite to travel
+            (this lets the sprite know if it needs
+            to slow down. we want it to slow
+            down before it gets to its target)
+        Returns:
+            bool
+        Parameters:
+            - self
+            - dist
+                this is the total distance from the
+                sprite to the target
+                can be any x,y value pair in
+                brackets [x,y] or parentheses (x,y)
+        '''
+        dist_x = dist[0] ** 2 # gets absolute value of the x distance
+        dist_y = dist[1] ** 2 # gets absolute value of the y distance
+        t_dist = dist_x + dist_y # gets total absolute value distance
+        speed = self.speed ** 2 # gets aboslute value of the speed
+
+        if t_dist <= (speed): # read function description above
+            return True
+        
+    def update(self):
+        '''
+        Function:
+            gets direction to move then applies
+            the distance to the sprite.center
+        Parameters:
+            - self
+        '''
+        self.dir = self.get_direction(self.target) # get direction
+        if self.dir: # if there is a direction to move
+            
+            if self.distance_check(self.dist): # if we need to slow down
+                if self.speed > 1:
+                    self.speed -= 1
+                else:
+                    self.speed = 1
+                self.speedX += (self.dir[0] * (self.speed / 2)) # reduced speed
+                self.speedY += (self.dir[1] * (self.speed / 2))
+                self.speedX *= self.slowing_friction # increased friction
+                self.speedY *= self.slowing_friction
+                
+            else: # if we need to go normal speed
+                self.speedX += (self.dir[0] * self.speed) # calculate speed from direction to move and speed constant
+                self.speedY += (self.dir[1] * self.speed)
+                self.speedX *= self.normal_friction # apply friction
+                self.speedY *= self.normal_friction
+
+            self.trueX += self.speedX # store true x decimal values
+            self.trueY += self.speedY
+            self.rect.center = ( round(self.trueX), round(self.trueY) ) # apply values to sprite.center
+
+                    
+ 
---
+++ pigraph/main.py
@@ -0,0 +1,110 @@
+import random
+
+import pygame
+from pygame.locals import *
+from igraph import *
+
+from display.colors import *
+from display.node import *
+# from display.inputbox import *
+
+class App:
+    """
+        Class:
+            Main Application draws graph animation
+        """
+
+    def __init__(self, width, height, graph_layout, filename):
+        self._running = True
+        self.screen = True
+        self.size = self.width, self.height = width, height
+        self.clock = pygame.time.Clock()
+        self.graph = Graph()
+        self.nodes = []
+        self.margin = 20
+        self.filename = filename
+        self.graph_layout = graph_layout
+
+
+    def on_init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size, 
+                             pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+        print "App initialized, window size: ", self.width, self.height
+
+        # draw input box
+        # query = inputbox.ask(self.screen, "Search")
+        # print query
+
+        try:
+            self.graph = load(self.filename)
+        except:
+            print "Couldn't load data file"
+            self._running = False
+            return
+
+        l = self.graph.layout(self.graph_layout)
+        l.fit_into( BoundingBox(self.width-(self.margin*2), self.height-(self.margin*2)) )
+        self.graph.vs["coords"] = l.coords
+        
+        for i, v in enumerate(self.graph.vs):
+            xpos = random.randrange(self.margin, self.width-self.margin, 1)
+            ypos = random.randrange(self.margin, self.height-self.margin, 1)
+            node = Node(i, xpos, ypos)
+            node.target = ( round(self.graph.vs[i]["coords"][0])+self.margin, round(self.graph.vs[i]["coords"][1])+self.margin )
+            self.nodes.append(node)
+            
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            print "App quitting"
+            self._running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                self._running = False
+
+
+    def on_render(self):
+        self.clock.tick(30)
+        pygame.display.set_caption("fps: " + str(self.clock.get_fps()))
+        self.screen.fill(gray)
+        for e in self.graph.es:        
+            src_idx = e.source
+            dest_idx = e.target
+            s_x = max(0, min(self.nodes[src_idx].trueX, self.width)) 
+            s_y = max(0, min(self.nodes[src_idx].trueY, self.height)) 
+            d_x = max(0, min(self.nodes[dest_idx].trueX, self.width)) 
+            d_y = max(0, min(self.nodes[dest_idx].trueY, self.height)) 
+            pygame.draw.line( self.screen, medgray, (s_x, s_y), (d_x, d_y) )  
+        for node in self.nodes:
+            node.update()
+            pygame.draw.ellipse(self.screen, blue, node.rect )            
+        pygame.display.flip()
+
+
+    def on_execute(self):
+        print "App executing"
+        if self.on_init() == False:
+            self._running = False
+        while (self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+    def on_loop(self):
+        pass
+
+
+    def on_cleanup(self):
+        print "App cleaning up"
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    theApp = App(1024, 768, "kamada_kawai", "data/file.pickle")
+    theApp.on_execute()
+
